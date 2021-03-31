4bdfbbff6abb5157f76f363f7cac92af59de85fe
shailmon <shailesh.monica24@gmail.com>
2021-03-31 22:23:30 +0530

    first

---
+++ PageObjectwithFactories/src/main/java/com/w2a/base/rough.java
@@ -0,0 +1,12 @@
+package com.w2a.base;
+
+public class rough {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s ="51,99,00";
+		System.out.println(s.replace(",", ""));
+	}
+
+}
